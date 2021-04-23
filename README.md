7f0d9a3440378c91d395f6b484a4e1b352011d77
Craig Drummond <craig.p.drummond@gmail.com>
2021-04-23 10:23:02 +0100

    Enable support for hardware buttons

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ configured, BT device.
 
 Code is forked/modified from https://github.com/oweitman/squeezelite-bluetooth/
 
+Prev/next and play/pause buttons on speakers are forwarded to LMS. Pass
+hostname/IP address of server as 1st argument to `btspeaker-monitor.py` - if
+none passed 'localhost' is assumed.
+
 Usage
 =====
 
@@ -13,6 +17,16 @@ Usage
 2. `chmod +x /usr/local/bluetooth/btspeaker-monitor.py /usr/local/bluetooth/check-squeezelite`
 2. Edit `bt-devices` to set LMS player name for a BT devices MAC address
 3. Pair BT devices if not already
+```
+sudo bluetoothctl
+[bluetooth]# scan on
+...
+[bluetooth]# scan off
+[bluetooth]# pair 00:00:00:00:00:00
+[bluetooth]# trust 00:00:00:00:00:00
+[bluetooth]# connect 00:00:00:00:00:00
+[bluetooth]# exit
+```
 4. Copy `btspeaker-monitor.service` to `/etc/systemd/system`
 5. `sudo daemon-reload`
 6. `sudo systemctl enable btspeaker-monitor`
--- btspeaker-monitor.py
+++ btspeaker-monitor.py
@@ -4,12 +4,16 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-#from gi.repository import GObject as gobject
-from gi.repository import GLib as glib
+from gi.repository import GLib
 
 import dbus
 import dbus.mainloop.glib
+import evdev
+import json
 import os
+import re
+import requests
+import sys
 from subprocess import Popen
 
 dbg = True
@@ -38,14 +42,108 @@ def controlChecker(val):
         checker=Popen([CHECKER], stdout=DEVNULL, stderr=DEVNULL, shell=False)
 
 
+# Input handling.......................
+DEV_DIR = '/dev/input'
+MAC_RE = re.compile('^([0-9A-F]{2}:){5}([0-9A-F]{2})$')
+
+deviceCheckTimeout = None
+
+def sendCommand(mac, cmd):
+    lms = 'localhost' if len(sys.argv)<2 else sys.argv[1]
+    url = 'http://%s:9000/jsonrpc.js' % lms
+    debug('Send %s to %s @ %s' % (str(cmd), mac.lower(), url))
+    try:
+        req = requests.post(url, json={'id':1, 'method':'slim.request', 'params': [mac.lower(), cmd]})
+        res = req.json()['result']
+        debug('Resp: %s' % str(res))
+        return res
+    except Exception as e:
+        debug("EX:%s" % str(e))
+
+
+def getDevices():
+    devices={}
+    for dev in list(map(evdev.device.InputDevice, evdev.util.list_devices(DEV_DIR))):
+        if dev.path is not None and dev.name is not None and MAC_RE.match(dev.name):
+            devices[dev.name.replace(':', '_')]=dev
+    return devices
+
+
+def handleInput(event, dev):
+    if (event.type in evdev.events.event_factory and evdev.events.event_factory[event.type] is evdev.events.KeyEvent):
+        keyEvent = evdev.events.KeyEvent(event)
+        if (keyEvent.keystate==evdev.events.KeyEvent.key_up):
+            if keyEvent.scancode in [evdev.ecodes.KEY_PLAYCD, evdev.ecodes.KEY_PLAY, evdev.ecodes.KEY_PLAYPAUSE, evdev.ecodes.KEY_PAUSE, evdev.ecodes.KEY_PAUSECD]:
+                resp = sendCommand(dev.name, ['mode', '?'])
+                if '_mode' in resp and resp['_mode']=='play':
+                    sendCommand(dev.name, ['pause'])
+                else:
+                    sendCommand(dev.name, ['play'])
+            elif keyEvent.scancode in [evdev.ecodes.KEY_STOP, evdev.ecodes.KEY_STOPCD]:
+                sendCommand(dev.name, ['stop'])
+            elif keyEvent.scancode == evdev.ecodes.KEY_NEXTSONG:
+                sendCommand(dev.name, ['playlist', 'index', '+1'])
+            elif keyEvent.scancode == evdev.ecodes.KEY_PREVIOUSSONG:
+                sendCommand(dev.name, ['button', 'jump_rew'])
+            else:
+                debug("Unhandled: %d" % keyEvent.scancode)
+
+
+def inputCallback(source, condition, dev):
+    #debug("inputCallback %s" % str(dev))
+    try:
+        event = dev.read_one()
+        while (event):
+            handleInput(event, dev)
+            event = dev.read_one()
+    except IOError:
+        # The device has no more data or the handle has been closed
+        pass
+    return True
+
+
+def openInputDev(key, dev):
+    debug("Open %s for %s" % (dev.path, key))
+    players[key]['input']['dev']=dev
+    players[key]['input']['watch']=GLib.io_add_watch(dev.fd, GLib.IO_IN, inputCallback, dev)
+
+
+def openInputs():
+    global deviceCheckTimeout
+    deviceCheckTimeout = None
+    for key in players:
+        if players[key]['input']['dev'] is None:
+            openInput(key)
+
+
+def openInput(key):
+    global deviceCheckTimeout
+    devices = getDevices()
+    players[key]['input']['checks']+=1
+    if key in devices:
+        openInputDev(key, devices[key])
+    elif deviceCheckTimeout is None and players[key]['input']['checks']<10:
+        debug("No dev for %s, check in 2s" % (key))
+        deviceCheckTimeout = GLib.timeout_add(2000, openInputs)
+
+
+def closeInput(key):
+    if players[key]['input']['dev'] is not None:
+        players[key]['input']['dev'].close()
+        GLib.source_remove(players[key]['input']['watch'])
+    players[key]['input']={'checks':0, 'dev':None, 'watch': None}
+#......................................
+
 def connected(hci, dev, name):
     key=dev.replace(':', '_')
     if key in players:
         return
 
     debug("Connected %s" % name,hci,dev)
-    players[key] = Popen([SQUEEZE_LITE, '-o', 'bluealsa:DEV=%s,PROFILE=a2dp' % (dev), '-n', name, '-m', dev, '-M', 'SqueezeLiteBT', '-f', '/dev/null'], stdout=DEVNULL, stderr=DEVNULL, shell=False)
+    players[key] = {'squeeze':Popen([SQUEEZE_LITE, '-s', 'localhost', '-o', 'bluealsa:DEV=%s,PROFILE=a2dp' % (dev), '-n', name, '-m', dev, '-M', 'SqueezeLiteBT', '-f', '/dev/null'], stdout=DEVNULL, stderr=DEVNULL, shell=False), 'input':{'checks':0, 'dev':None, 'watch': None}}
+    openInput(key)
     controlChecker(1)
+
 
 def disconnected(dev, name):
     key=dev.replace(':', '_')
@@ -53,10 +151,12 @@ def disconnected(dev, name):
         return
 
     debug("Disconnected %s" % name,dev)
-    players[key].kill()
-    os.waitpid(players[key].pid, 0)
+    players[key]['squeeze'].kill()
+    os.waitpid(players[key]['squeeze'].pid, 0)
+    closeInput(key)
     players.pop(key)
     controlChecker(-1)
+
 
 def getName(dev):
     with open(CONFIG_FILE) as f:
@@ -66,34 +166,8 @@ def getName(dev):
                 return parts[1] 
     return None
 
-def bluealsa_handler(path, *args, **kwargs):
-    """Catch all handler.
-    Catch and debug information about all signals.
-    """ 
-    dbus_interface = kwargs['dbus_interface']
-    member = kwargs['member']
-    dev = None
-    hci = None
-    if path:
-        parts=path.split('/')
-        if len(parts)>=4:
-            hci=parts[3]
-            dev=":".join(parts[4].split('_')[1:])
 
-    name = None
-    if None!=dev and None!=hci:
-        name = getName(dev)
-
-    if None==name:
-        debug("Unknown device") 
-    else:
-        if member == "PCMRemoved" :
-            disconnected(dev, name)
-        elif member == "PCMAdded" :
-            connected(hci, dev, name)
-
-
-def catchall_handler(name, attr, *args, **kwargs):
+def catchallHandler(name, attr, *args, **kwargs):
     """Catch all handler.
     Catch and debug information about all signals.
     """
@@ -125,10 +199,7 @@ if __name__ == '__main__':
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
     bus = dbus.SystemBus()
-    #bus.add_signal_receiver(bluealsa_handler, dbus_interface="org.bluealsa.Manager1", interface_keyword='dbus_interface', member_keyword='member')
-    bus.add_signal_receiver(catchall_handler, bus_name="org.bluez", interface_keyword='dbus_interface', member_keyword='member', path_keyword='path')
+    bus.add_signal_receiver(catchallHandler, bus_name="org.bluez", interface_keyword='dbus_interface', member_keyword='member', path_keyword='path')
 
-
-    #mainloop = gobject.MainLoop()
-    mainloop = glib.MainLoop()
+    mainloop = GLib.MainLoop()
     mainloop.run()
